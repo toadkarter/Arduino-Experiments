06d60a6f2649ebd79092e187d66e27dabaaeba1b
toadkarter <vrakhmanin1@gmail.com>
2022-02-18 23:03:29 +0000

    Moved delay up to blink together function

--- flashingLights/src/main.cpp
+++ flashingLights/src/main.cpp
@@ -2,8 +2,8 @@
 
 int ledPin[4] = {7, 8, 12, 13};
 void blinkTogether(int ledPin[], int speed);
-void allLightsOn(int ledPin[], int speed);
-void allLightsOff(int ledPin[], int speed);
+void allLightsOn(int ledPin[]);
+void allLightsOff(int ledPin[]);
 
 void setup() {
   for (size_t i = 0; i < 4; i++)
@@ -17,23 +17,22 @@ void loop() {
 }
 
 void blinkTogether(int ledPin[], int speed) {
-  allLightsOn(ledPin, speed);
-  allLightsOff(ledPin, speed);
+  allLightsOn(ledPin);
+  delay(speed);
+  allLightsOff(ledPin);
+  delay(speed);
 }
 
-void allLightsOn(int ledPin[], int speed) {
+void allLightsOn(int ledPin[]) {
   for (size_t i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], HIGH);
   }
-  delay(speed);
 }
 
-void allLightsOff(int ledPin[], int speed) {
+void allLightsOff(int ledPin[]) {
   for (size_t i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], LOW);
   }
-  delay(speed);
-
 }
