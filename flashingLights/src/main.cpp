67bee6369d68adc8d23d50e0b0c969b986917f4a
toadkarter <vrakhmanin1@gmail.com>
2022-02-18 22:57:17 +0000

    Refactored blink together function

--- flashingLights/src/main.cpp
+++ flashingLights/src/main.cpp
@@ -2,6 +2,8 @@
 
 int ledPin[4] = {7, 8, 12, 13};
 void blinkTogether(int ledPin[]);
+void allLightsOn(int ledPin[]);
+void allLightsOff(int ledPin[]);
 
 void setup() {
   for (size_t i = 0; i < 4; i++)
@@ -15,14 +17,23 @@ void loop() {
 }
 
 void blinkTogether(int ledPin[]) {
+  allLightsOn(ledPin);
+  allLightsOff(ledPin);
+}
+
+void allLightsOn(int ledPin[]) {
   for (size_t i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], HIGH);
   }
   delay(1000);
+}
+
+void allLightsOff(int ledPin[]) {
   for (size_t i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], LOW);
   }
   delay(1000);
+
 }
