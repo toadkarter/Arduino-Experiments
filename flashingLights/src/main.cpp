6e7803b1ec0513eabaa6e96adf67a5fc8dd5314a
toadkarter <vrakhmanin1@gmail.com>
2022-02-18 23:19:35 +0000

    Added left to right function

--- flashingLights/src/main.cpp
+++ flashingLights/src/main.cpp
@@ -5,6 +5,8 @@ void setBulbsToOutput(int ledPin[]);
 void blinkTogether(int ledPin[], int speed);
 void allLightsOn(int ledPin[]);
 void allLightsOff(int ledPin[]);
+void selectOneBulb(int ledPin[], int bulbNumber);
+void lightsLeftToRight(int ledPin[], int speed);
 
 void setup() {
   setBulbsToOutput(ledPin);
@@ -12,10 +14,11 @@ void setup() {
 
 void loop() {
   blinkTogether(ledPin, 1000);
+  lightsLeftToRight(ledPin, 500);
 }
 
 void setBulbsToOutput(int ledPin[]) {
-  for (size_t i = 0; i < 4; i++)
+  for (int i = 0; i < 4; i++)
   {
     pinMode(ledPin[i], OUTPUT);
   }
@@ -29,15 +32,36 @@ void blinkTogether(int ledPin[], int speed) {
 }
 
 void allLightsOn(int ledPin[]) {
-  for (size_t i = 0; i < 4; i++)
+  for (int i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], HIGH);
   }
 }
 
 void allLightsOff(int ledPin[]) {
-  for (size_t i = 0; i < 4; i++)
+  for (int i = 0; i < 4; i++)
   {
     digitalWrite(ledPin[i], LOW);
   }
 }
+
+void lightsLeftToRight(int ledPin[], int speed) {
+  for (int i = 0; i < 4; i++) {
+    selectOneBulb(ledPin, i);
+    delay(speed);
+  }
+  allLightsOff(ledPin);
+  delay(speed);
+}
+
+void selectOneBulb(int ledPin[], int bulbNumber) {
+  for (int i = 0; i < 4; i++) {
+    if (i == bulbNumber) {
+      digitalWrite(ledPin[i], HIGH);
+    } else {
+      digitalWrite(ledPin[i], LOW);
+    }
+  }
+}
+
+  
